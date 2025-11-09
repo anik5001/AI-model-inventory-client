18193de38b9c66fc4bb13a06f3caa7b8c45b08c2
anik5001 <aniksarker047@gmail.com>
2025-11-10 02:09:23 +0600

    delete model complete

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,23 +1,42 @@
 import React, { use, useEffect, useState } from "react";
-import { Link, useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import { AuthContext } from "../context/AuthContext";
 
 const DetailsPage = () => {
   const { user } = use(AuthContext);
   const { id } = useParams();
   const [model, setModel] = useState({});
+  const navigate = useNavigate();
   // console.log(id);
   useEffect(() => {
     fetch(`http://localhost:3000/models/${id}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         setModel(data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+
+  const handleDeleteModel = () => {
+    fetch(`http://localhost:3000/models/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        alert("delete successful");
+        navigate("/all-models");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="max-w-6xl mx-auto p-4 md:p-6 lg:p-8">
       <div className="card bg-base-100 shadow-xl border border-gray-200 rounded-2xl overflow-hidden">
@@ -73,7 +92,7 @@ const DetailsPage = () => {
                   </Link>
 
                   <button
-                    // onClick={handleDlete}
+                    onClick={handleDeleteModel}
                     className="btn btn-secondary rounded-full "
                   >
                     Delete
